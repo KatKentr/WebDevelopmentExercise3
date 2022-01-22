65b0f3d7af1ee58f511baa169d9c755bb2fd84b5
Katerina <kkentroti@gmail.com>
2022-01-22 13:34:10 +0200

    functionalities from previous assignement added

--- preferences.js
+++ preferences.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 showData();                             //current preferences are displayed 
 
@@ -6,12 +6,14 @@ updateFields();
 
 let submitData = document.getElementById('InsertSetup');
 
+const table=document.querySelector('table');
 
 submitData.addEventListener('click', () => {         //when the user clicks on Insert, local storage and table is updated
     addSetUpData();
     addTableData();
     deleteTable();        //remove current table before create a new one
     updateFields();
+    location.reload();
 })
 
 function addSetUpData() {                                              //add data to localStrorage
@@ -37,7 +39,7 @@ function addTableData(){
         console.log("rows "+rows.length,"columns " +headers.length);
         console.log(inputElements[0],typeof(inputElements));
 
-        for (i=0; i<inputElements.length;i++){    //strore the values of the input fields in arr2
+        for (var i=0; i<inputElements.length;i++){    //strore the values of the input fields in arr2
             arr2.push(inputElements[i].value);          
         }
         
@@ -75,8 +77,8 @@ function showData() {
     var tableData=getTableData();
     console.log(tableData);
     var inputElements=document.getElementsByClassName('inputField');
-
-    for (i=0; i<inputElements.length;i++){    //assign to the input value the values from local Storage
+       
+    for (var i=0; i<inputElements.length;i++){    //assign to the input value the values from local Storage
         inputElements[i].value=tableData[i];          
     }
 
@@ -92,12 +94,14 @@ function updateTitle(title) {
 
 function deleteTable(){
     var tobody=document.getElementById('tableBody');
+    var footer=document.getElementById('tableFooter');
      if (tableHasBody()){
         
         console.log("tbody is:"+tobody);
         tobody.remove();
-     }
         
+     }
+     footer.remove();  
 }
 
 function tableHasBody(){
@@ -119,14 +123,14 @@ function createTable(table, rowCount, columns) {
         var tbody = document.createElement('tbody');                 //add tbody element
         tbody.id = "tableBody"
 
-        for (i = 0; i < rowCount; i++) {            //create rows and add cells
+        for (var i = 0; i < rowCount; i++) {            //create rows and add cells
             cell[i] = 0;
             inputElements[i] = 0;
             console.log(i);
           
             var row =document.createElement("tr");       //cretae rows
 
-            for (j = 0; j < columns; j++) {
+            for (var j = 0; j < columns; j++) {
   
 
                 var cell=document.createElement('td');           //create cells for each row(td elements)
@@ -167,6 +171,10 @@ function createTable(table, rowCount, columns) {
             table.appendChild(tbody); 
         }
     }
+    
+    const footerStrings=["Best: ", "Worst: ", "Average: "];
+    //call the function to create footer
+    createFooter(footerStrings,Statistics,table);
 }
 
 
@@ -191,9 +199,227 @@ function updateFields() {
 
     updateTitle(title);
     
+    
     createTable(table, rows, colCount);                 //create a table if the input is valid 
     
-    const footerStrings=["Best: ", "Worst: ", "Average: "];
+   
     
 
 }
+
+//function to calculate min, max and average score
+function Statistics(table){
+    //array that holds the scores
+    let rows = table.rows;
+    let score = [];
+    let sum = 0;
+     let max = 0;
+     let min = 7000; //initializing with an unrealistic value
+    for (var i = 1; i < (rows.length - 1); i++) {
+      let x = rows[i].getElementsByClassName('inputField')[2];
+      score[i]= parseFloat((x.value));  //converting string to number
+      sum+= score[i];
+      //console.log("score is: ",score[i],typeof score[i]);
+      if (score[i]>max){
+        max = score[i];
+      }
+      if (score[i]<min){
+        min = score[i];
+      } 
+    }
+    //console.log(score.length); 
+    let avg=Math.round((sum/score.length));
+    console.log(sum);
+    if (score.length===0 || (Number.isNaN(sum))){
+      return [0,0,0]
+    } else{
+      return [max,min,avg];
+    }
+   
+  }
+
+  const footerStrings=["Best: ", "Worst: ", "Average: "];
+//function to create a tfoot element with 3 cells
+ function createFooter(stringsArray,Statistics,table){
+  let z = Statistics(table);
+  var footer = table.createTFoot();
+  footer.id="tableFooter";
+  var row = footer.insertRow(0);
+  for (var i=0; i<3; i++ ){
+    var cell = row.insertCell(i);
+    cell.style.width="100px";
+    cell.style.fontWeight='bold';
+    cell.innerHTML = stringsArray[i] +(z[i]);
+  } 
+}
+
+//function to update the values in the footer
+function updateFooter(stringsArray,Statistics,table){
+    let rows = table.rows;
+    let z = Statistics(table);
+    let footRowIndex = rows.length - 1;
+    for (var i=0;i<3;i++)
+      table.rows[footRowIndex].cells[i].innerHTML=stringsArray[i] +(z[i]);
+  }
+
+  //add event to sort the table when the user clicks a header in ascending and descending order
+
+table.querySelectorAll('th').forEach((element,columnNo)=>{
+    element.addEventListener('click',function(event){
+      sortTable(table,columnNo);
+    })
+})
+
+//event delegation
+
+document.querySelector('tbody').addEventListener('click',function (e){
+  if (e.target && e.target.matches("button.deleteRow")) {    //if the delete button of a row is clicked this row is deleted
+        //console.log("Delete button clicked!");
+        const rows = document.querySelectorAll('tr');
+        const rowsArray = Array.from(rows);
+        const rowIndex = rowsArray.findIndex(row => row.contains(event.target));
+        deleteRow(table,rowIndex);
+        
+	} else if(e.target && e.target.matches("button.copyRow")){   //if the copy button of a row is clicked this row is  duplicated
+        //console.log("Copy button clicked!");
+         const rows = document.querySelectorAll('tr');
+        const rowsArray = Array.from(rows);
+        const rowIndex = rowsArray.findIndex(row => row.contains(event.target));
+        copyRow(table,rowIndex);
+        
+  }
+  updateFooter(footerStrings,Statistics,table); //updating the statistics in the tfoot after an insertion or a remove from the table
+})
+
+
+//function for duplicating row
+function copyRow(table,rowIndex){
+  var clone = table.rows[rowIndex].cloneNode(true); // copy children too
+  insertAfter(table.rows[rowIndex],clone); //add the duplicated row after the row
+}
+
+//function for deleting a row
+function deleteRow(table,rowIndex){
+
+  table.deleteRow(rowIndex);
+  //console.log(rowIndex);
+  
+}
+
+//function for inserting an element after another element
+function insertAfter(after, newNode){
+  after.parentNode.insertBefore(newNode, after.nextSibling);
+}
+
+
+//function for sorting the table
+ function sortTable(table,columnNo){
+
+  //storing te th elements(the header in the table) in an array
+  let headerTitles=document.getElementsByTagName('th');
+  
+  let rows,switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first and the last, which contains table headers and the tfoot): */
+    for (i = 1; i < (rows.length - 2); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* comparing two elements from a row and from the next row */
+
+       x = rows[i].getElementsByClassName('inputField')[columnNo];
+       y = rows[i + 1].getElementsByClassName('inputField')[columnNo];
+       
+      //prints for debugging purposes
+      // console.log(x,y);
+      // console.log(typeof x);
+      // console.log(typeof x.value);
+
+      //turn the font of the text to bold
+      //x.style.fontWeight='bold';
+      //y.style.fontWeight='bold';
+
+      /* Check if the two rows should switch place by comparing the value attributes of the input elements x,y
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+      
+        if (headerTitles[columnNo].innerText == "Athlete"){  //first case: sorting based on surname
+
+          //split the string to sort according to surname
+          var name1 = x.value.split(" ");
+          var name2 = y.value.split(" ");
+
+          if (name1[1].toLowerCase() > name2[1].toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+            
+          }
+        }else if(headerTitles[columnNo].innerText=="Country"){ //second case: sorting of strings (country name)
+          if (x.value.toLowerCase() > y.value.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }else{
+          if (parseFloat(x.value) > parseFloat(y.value)){      //third case: sort scores (number)
+            shouldSwitch = true;
+            break;
+          }
+        }
+        
+      } else if (dir == "desc") {
+        if (headerTitles[columnNo].innerText == "Athlete"){
+         //split the string to sort according to surname
+         var name1 = x.value.split(" ");
+         var name2 = y.value.split(" ");
+
+         if (name1[1].toLowerCase() < name2[1].toLowerCase()) {
+           // If so, mark as a switch and break the loop:
+           shouldSwitch = true;
+           break;
+           
+         }
+       } else if(headerTitles[columnNo].innerText== "Country") {
+        if (x.value.toLowerCase() < y.value.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+        
+      }else{
+        if (parseFloat(x.value)< parseFloat(y.value)) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+      }
+      }
+    }
+  }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+    
+  }
+}
+
